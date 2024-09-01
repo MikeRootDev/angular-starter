73cb99e5138273269f9b292ac6c849f0559e087f
Mike Root <michael.root@veteransunited.com>
2024-08-31 22:53:15 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { EmployeesComponent } from './employees/employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'jobs',
+    component: JobsComponent,
+    title: 'Jobs',
+  },
+  {
+    path: 'employees',
+    component: EmployeesComponent,
+    title: 'Employees',
+  },
+];
